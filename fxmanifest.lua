79bb97b640b0ee3d1e7dbe646d5819a616cd7d09
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-02-11 05:58:17 -0600

    Description for txAdmin

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-
-description 'qb-loading'
+lua54 'yes'
+author 'Kakarot'
+description 'Loading screen'
 version '1.2.0'
-
 
 files {
   'assets/**',
@@ -16,5 +16,3 @@ loadscreen {
 
 loadscreen_cursor 'yes'
 loadscreen_manual_shutdown 'yes'
-
-lua54 'yes'
